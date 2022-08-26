574da907e6f356ed9a2345def769072f5e460664
bonnnnnnn <whateversit@gmail.com>
2022-08-26 11:22:02 +0700

    add class-props.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import {Header,Content,Footer} from './func-components'
+import MsgBox from './class-props';
 
 function App() {
   return (
-    [<Header/>,<Content/>,<Footer/>]
+    //[<Header/>,<Content/>,<Footer/>]
+    <MsgBox
+      text="React & React Native"
+      color="blue"
+      bgColor="#ccc"
+      fontSize="18pt"
+      border="solid 1px black"
+    />
   );
 }
 
