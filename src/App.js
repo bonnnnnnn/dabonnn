9ea3afadef2096aaae7bbea9dc6ed84934a8187f
bonnnnnnn <whateversit@gmail.com>
2022-08-26 10:55:23 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {Header,Content,Footer} from './func-components'
+
+function App() {
+  return (
+    [<Header/>,<Content/>,<Footer/>]
+  );
+}
+
+export default App;
